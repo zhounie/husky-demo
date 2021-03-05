4febf75932169893ba58291323033b5ad866e15f
zhounie <iszhounie@aliyun.com>
2021-03-05 10:42:41 +0800

    feat: add test

---
+++ src/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="test">
+      hhhhhh
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hhhhhh'
+    };
+  },
+  methods: {
+    click() {
+      this.msg = 'uuu';
+    }
+  }
+};
+</script>
+
+<style>
+.test {
+  width: 200px;
+  height: 200px;
+}
+</style>
